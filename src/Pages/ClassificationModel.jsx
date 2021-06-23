33db6e54c62fb90322c9de7fbd655a0036312b54
ayelet97 <ayeletush10@gmail.com>
2021-06-23 05:09:04 +0300

    Add attacks graph

--- src/Pages/ClassificationModel.jsx
+++ src/Pages/ClassificationModel.jsx
@@ -14,6 +14,7 @@ class ClassificationModel extends Component {
 		alert("Date: " + e.dataPoint.x);
 		this.weather_chart.options.data[0].dataPoints.push({ x: new Date("2019- 01- 01"), y: [19, 26] })
 
+		this.attacks_chart.render();
 		this.weather_chart.render();
 		this.holidays_chart.render();
 		this.google_trends_israel_chart.render();
@@ -28,7 +29,10 @@ class ClassificationModel extends Component {
 			e.dataSeries.visible = true;
 		}
 
-		if (e.chart.title.text === "Weather") {
+		if (e.chart.title.text === "Terror Attacks") {
+			this.attacks_chart.render();
+		}
+		else if (e.chart.title.text === "Weather") {
 			this.weather_chart.render();
 		}
 		else if (e.chart.title.text === "Google Trends - Israel") {
@@ -51,63 +55,111 @@ class ClassificationModel extends Component {
 		return dps;
 	}
 
+	getNumAttacksDataPoints(date) {
+		var dps = []
+		var year = 2019
+		var month = 1
+		var day, value
+		for (day = 1; day <= 30; day++) {
+			value = Math.floor(Math.random() * 3);
+			dps.push({x: new Date(year, month, day), y: value})
+		}
+		return dps;
+	}
+
+	getNumDeathsDataPoints(date) {
+		var dps = []
+		var year = 2019
+		var month = 1
+		var day, value
+		for (day = 1; day <= 30; day++) {
+			value = Math.floor(Math.random() * 3);
+			dps.push({x: new Date(year, month, day), y: value})
+		}
+		return dps;
+	}
+
+	getNumWoundedDataPoints(date) {
+		var dps = []
+		var year = 2019
+		var month = 1
+		var day, value
+		for (day = 1; day <= 30; day++) {
+			value = Math.floor(Math.random() * 3);
+			dps.push({x: new Date(year, month, day), y: value})
+		}
+		return dps;
+	}
+
 	// Get temperature data of the 30 days before the selected date
 	getTemperatureDataPoints(date) {
 		var dps = [
-			{ x: new Date("2017- 01- 01"), y: [19, 26] },
-			{ x: new Date("2017- 02- 01"), y: [19, 26] },
-			{ x: new Date("2017- 03- 01"), y: [18, 25] },
-			{ x: new Date("2017- 04- 01"), y: [15, 23] },
-			{ x: new Date("2017- 05- 01"), y: [12, 20] },
-			{ x: new Date("2017- 06- 01"), y: [10, 18] },
-			{ x: new Date("2017- 07- 01"), y: [8, 17] },
-			{ x: new Date("2017- 08- 01"), y: [9, 18] },
-			{ x: new Date("2017- 09- 01"), y: [12, 20] },
-			{ x: new Date("2017- 10- 01"), y: [14, 22] },
-			{ x: new Date("2017- 11- 01"), y: [16, 24] },
-			{ x: new Date("2017- 12- 01"), y: [18, 26] },
-			{ x: new Date("2018- 01- 01"), y: [19, 26] },
-			{ x: new Date("2018- 02- 01"), y: [19, 26] },
-			{ x: new Date("2018- 03- 01"), y: [18, 25] },
-			{ x: new Date("2018- 04- 01"), y: [15, 23] },
-			{ x: new Date("2018- 05- 01"), y: [12, 20] },
-			{ x: new Date("2018- 06- 01"), y: [10, 18] },
-			{ x: new Date("2018- 07- 01"), y: [8, 17] },
-			{ x: new Date("2018- 08- 01"), y: [9, 18] },
-			{ x: new Date("2018- 09- 01"), y: [12, 20] },
-			{ x: new Date("2018- 10- 01"), y: [14, 22] },
-			{ x: new Date("2018- 11- 01"), y: [16, 24] },
-			{ x: new Date("2018- 12- 01"), y: [18, 26] }
+			{ x: new Date("2019- 01- 01"), y: [19, 26] },
+			{ x: new Date("2019- 01- 02"), y: [19, 26] },
+			{ x: new Date("2019- 01- 03"), y: [18, 25] },
+			{ x: new Date("2019- 01- 04"), y: [15, 23] },
+			{ x: new Date("2019- 01- 05"), y: [12, 20] },
+			{ x: new Date("2019- 01- 06"), y: [10, 18] },
+			{ x: new Date("2019- 01- 07"), y: [8, 17] },
+			{ x: new Date("2019- 01- 08"), y: [9, 18] },
+			{ x: new Date("2019- 01- 09"), y: [12, 20] },
+			{ x: new Date("2019- 01- 10"), y: [14, 22] },
+			{ x: new Date("2019- 01- 11"), y: [16, 24] },
+			{ x: new Date("2019- 01- 12"), y: [18, 26] },
+			{ x: new Date("2019- 01- 13"), y: [19, 26] },
+			{ x: new Date("2019- 01- 14"), y: [19, 26] },
+			{ x: new Date("2019- 01- 15"), y: [18, 25] },
+			{ x: new Date("2019- 01- 16"), y: [15, 23] },
+			{ x: new Date("2019- 01- 17"), y: [12, 20] },
+			{ x: new Date("2019- 01- 18"), y: [10, 18] },
+			{ x: new Date("2019- 01- 19"), y: [8, 17] },
+			{ x: new Date("2019- 01- 20"), y: [9, 18] },
+			{ x: new Date("2019- 01- 21"), y: [12, 20] },
+			{ x: new Date("2019- 01- 22"), y: [14, 22] },
+			{ x: new Date("2019- 01- 23"), y: [16, 24] },
+			{ x: new Date("2019- 01- 24"), y: [18, 26] },
+			{ x: new Date("2019- 01- 25"), y: [18, 27] },
+			{ x: new Date("2019- 01- 26"), y: [14, 22] },
+			{ x: new Date("2019- 01- 27"), y: [13, 23] },
+			{ x: new Date("2019- 01- 28"), y: [18, 26] },
+			{ x: new Date("2019- 01- 29"), y: [18, 27] },
+			{ x: new Date("2019- 01- 30"), y: [20, 29] }
 		]
 		return dps;
 	}
 
 	getPrecipitationDataPoints(date) {
 		var dps = [
-			{ x: new Date("2017- 01- 01"), y: 0 },
-			{ x: new Date("2017- 02- 01"), y: 0 },
-			{ x: new Date("2017- 03- 01"), y: 0 },
-			{ x: new Date("2017- 04- 01"), y: 3.4 },
-			{ x: new Date("2017- 05- 01"), y: 1 },
-			{ x: new Date("2017- 06- 01"), y: 4 },
-			{ x: new Date("2017- 07- 01"), y: 0 },
-			{ x: new Date("2017- 08- 01"), y: 0 },
-			{ x: new Date("2017- 09- 01"), y: 0 },
-			{ x: new Date("2017- 10- 01"), y: 1.2 },
-			{ x: new Date("2017- 11- 01"), y: 1.5 },
-			{ x: new Date("2017- 12- 01"), y: 0 },
-			{ x: new Date("2018- 01- 01"), y: 0 },
-			{ x: new Date("2018- 02- 01"), y: 0 },
-			{ x: new Date("2018- 03- 01"), y: 0 },
-			{ x: new Date("2018- 04- 01"), y: 0 },
-			{ x: new Date("2018- 05- 01"), y: 0 },
-			{ x: new Date("2018- 06- 01"), y: 0 },
-			{ x: new Date("2018- 07- 01"), y: 0 },
-			{ x: new Date("2018- 08- 01"), y: 0 },
-			{ x: new Date("2018- 09- 01"), y: 0 },
-			{ x: new Date("2018- 10- 01"), y: 0 },
-			{ x: new Date("2018- 11- 01"), y: 0 },
-			{ x: new Date("2018- 12- 01"), y: 0 }
+			{ x: new Date("2019- 01- 01"), y: 0},
+			{ x: new Date("2019- 01- 02"), y: 0 },
+			{ x: new Date("2019- 01- 03"), y: 0 },
+			{ x: new Date("2019- 01- 04"), y: 1.2 },
+			{ x: new Date("2019- 01- 05"), y: 0},
+			{ x: new Date("2019- 01- 06"), y: 0 },
+			{ x: new Date("2019- 01- 07"), y: 3 },
+			{ x: new Date("2019- 01- 08"), y: 4.5 },
+			{ x: new Date("2019- 01- 09"), y: 3 },
+			{ x: new Date("2019- 01- 10"), y: 0 },
+			{ x: new Date("2019- 01- 11"), y: 0 },
+			{ x: new Date("2019- 01- 12"), y: 0 },
+			{ x: new Date("2019- 01- 13"), y: 0 },
+			{ x: new Date("2019- 01- 14"), y: 0 },
+			{ x: new Date("2019- 01- 15"), y: 0 },
+			{ x: new Date("2019- 01- 16"), y: 0.9 },
+			{ x: new Date("2019- 01- 17"), y: 1.2 },
+			{ x: new Date("2019- 01- 18"), y: 0 },
+			{ x: new Date("2019- 01- 19"), y: 0 },
+			{ x: new Date("2019- 01- 20"), y: 0 },
+			{ x: new Date("2019- 01- 21"), y: 0 },
+			{ x: new Date("2019- 01- 22"), y: 0 },
+			{ x: new Date("2019- 01- 23"), y: 0 },
+			{ x: new Date("2019- 01- 24"), y: 0 },
+			{ x: new Date("2019- 01- 25"), y: 5.4 },
+			{ x: new Date("2019- 01- 26"), y: 4.8 },
+			{ x: new Date("2019- 01- 27"), y: 3.3 },
+			{ x: new Date("2019- 01- 28"), y: 0 },
+			{ x: new Date("2019- 01- 29"), y: 0 },
+			{ x: new Date("2019- 01- 30"), y: 0 }
 		]
 		return dps;
 	}
@@ -121,7 +173,7 @@ class ClassificationModel extends Component {
 
 		var dps = []
 		var year = 2019
-		var month = 2
+		var month = 1
 		var day, value
 		for (day = 1; day <= 30; day++) {
 			value = Math.floor(Math.random() * 100);
@@ -160,6 +212,57 @@ class ClassificationModel extends Component {
 				click: this.updateCharts
 			}]
 		}
+
+		const attacks = {
+			height: 200,
+			exportEnabled: true,
+			exportFileName: "Attacks_Graph",
+			animationEnabled: true,
+			zoomEnabled: true,
+			theme: "light2",
+			title: {
+				text: "Terror Attacks",
+				fontFamily: "Candara",
+				fontWeight: "bold",
+                fontSize: 20
+			},
+			axisY: {
+				title: "Information",
+				titleFontFamily: "Candara",
+				titleFontWeight: "bold",
+				includeZero: true
+			},
+			toolTip: {
+				shared: true,
+				fontFamily: "Candara"
+			},
+			legend: {
+				fontFamily: "Candara",
+				cursor: "pointer",
+				itemclick: this.toggleDataSeries
+			},
+			data: [{
+				type: "column",
+				name: "Attacks",
+				showInLegend: true,
+				toolTipContent: "<strong>{x}</strong></br> Attacks: {y}",
+				dataPoints: this.getNumAttacksDataPoints(2019)
+			},
+			{
+				type: "column",
+				name: "Deaths",
+				showInLegend: true,
+				toolTipContent: "Deaths: {y}",
+				dataPoints: this.getNumDeathsDataPoints(2019)
+			},
+			{
+				type: "column",
+				name: "Wounded",
+				showInLegend: true,
+				toolTipContent: "Wounded: {y}",
+				dataPoints: this.getNumWoundedDataPoints(2019)
+			}]
+		}
 		
 		const weather = {
 			height: 200,
@@ -172,7 +275,7 @@ class ClassificationModel extends Component {
 				text: "Weather",
 				fontFamily: "Candara",
 				fontWeight: "bold",
-                fontSize: 20,
+                fontSize: 20
 			},
 			axisY: {
 				title: "Temperature (°C)",
@@ -210,7 +313,6 @@ class ClassificationModel extends Component {
 				name: "Temperature",
 				showInLegend: true,
 				indexLabel: "{y[#index]}°",
-				xValueFormatString: "MMM YYYY",
 				toolTipContent: "<strong>{x}</strong></br> Max Temperature: {y[1]}°C<br/> Min Temperature: {y[0]}°C",
 				dataPoints: this.getTemperatureDataPoints(2019)
 			},
@@ -219,7 +321,6 @@ class ClassificationModel extends Component {
 				name: "Precipitation",
 				axisYType: "secondary",
 				showInLegend: true,
-				xValueFormatString: "MMM YYYY",
 				toolTipContent: "Precipitation: {y} cm",
 				dataPoints: this.getPrecipitationDataPoints(2019)
 			}]
@@ -389,7 +490,6 @@ class ClassificationModel extends Component {
 			data: [{
 				type: "rangeColumn",
 				indexLabel: "{y[#index]}°",
-				xValueFormatString: "MMM YYYY",
 				toolTipContent: "<strong>{x}</strong></br> Max: {y[1]}°C<br/> Min: {y[0]}°C",
 				dataPoints: this.getTemperatureDataPoints(2019)
 			}]
@@ -407,9 +507,9 @@ class ClassificationModel extends Component {
 				onRef={ref => this.weather_chart = ref}
 				/>
 			</div>
-			<div id = "holidays_chart" style= {{display: 'inline-block', width: '45%', height: '100%', paddingTop: '5px', paddingBottom: '5px', marginLleft: 'auto', marginRight: 'auto', border: '1px solid black'}}>
-				<CanvasJSChart options = {holidays}
-				onRef={ref => this.holidays_chart = ref}
+			<div id = "attacks_chart" style= {{display: 'inline-block', width: '45%', height: '100%', paddingTop: '5px', paddingBottom: '5px', marginLleft: 'auto', marginRight: 'auto', border: '1px solid black'}}>
+				<CanvasJSChart options = {attacks}
+				onRef={ref => this.attacks_chart = ref}
 				/>
 			</div>
 			<div id = "google_trends_israel_chart" style= {{display: 'inline-block', width: '45%', height: '100%', paddingTop: '5px', paddingBottom: '5px', marginTop: '10px', marginLleft: 'auto', marginRight: '15px', border: '1px solid black'}}>
@@ -420,6 +520,13 @@ class ClassificationModel extends Component {
 			<div id = "google_trends_palestine_chart" style= {{display: 'inline-block', width: '45%', height: '100%', paddingTop: '5px', paddingBottom: '5px', marginTop: '10px', marginLleft: 'auto', marginRight: 'auto', border: '1px solid black'}}>
 				<CanvasJSChart options = {googleTrendsPalestine}
 				onRef={ref => this.google_trends_palestine_chart = ref}
+				/>
+			</div>
+
+
+			<div id = "holidays_chart" style= {{display: 'inline-block', width: '45%', height: '100%', paddingTop: '5px', paddingBottom: '5px', marginTop: '10px', marginLleft: 'auto', marginRight: '15px', border: '1px solid black'}}>
+				<CanvasJSChart options = {holidays}
+				onRef={ref => this.holidays_chart = ref}
 				/>
 			</div>
 		</div>
