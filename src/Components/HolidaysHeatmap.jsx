ec4b58611ab78162b94b37f442995837cf29f9de
ayelet97 <ayeletush10@gmail.com>
2021-09-05 15:17:28 +0300

    Connect holidays heatmap to db

--- src/Components/HolidaysHeatmap.jsx
+++ src/Components/HolidaysHeatmap.jsx
@@ -8,16 +8,13 @@ class HolidaysHeatmap extends Component {
     constructor() {
         super();
         this.state = {
-            startDate: new Date("2016/01/12"),
-            endDate: new Date("2016/02/11"),
             dataPoints: [],
             muslimHolidays: [],
             jewishHolidays: [],
             christianHolidays: []
         };
     }
-
-    /*
+   
     componentDidMount() {
         this.getDataPoints();
     }
@@ -27,100 +24,104 @@ class HolidaysHeatmap extends Component {
             this.getDataPoints();
         }
     }
-    */
-
+    
     async getDataPoints() {
         let response = await axios.get("/Holidays", {
             params: {
                 startDate : this.props.startDate,
                 endDate : this.props.endDate
             }
-          });
+        });
         var dps = [], muslimDps = [], jewishDps = [], christianDps = [];
-        var count = 0;
         response.data.map((holidaysData) => {
+            var count = 0;
+            var date = new Date(holidaysData.date)
+		    date.setDate(date.getDate());
+		    var formattedDate = date.toISOString().slice(0, 10);
+            formattedDate = formattedDate.split('-').join('/');
+
             // Muslim holidays
             if (holidaysData.Eid_al_Fitr === 1) {
-                muslimDps.push({ date: holidaysData.date, holiday: "Eid al Fitr" });
+                muslimDps.push({ date: formattedDate, holiday: "Eid al Fitr" });
                 count += 1;
             }
             if (holidaysData.Arafat_Day === 1) {
-                muslimDps.push({ date: holidaysData.date, holiday: "Arafat Day" });
+                muslimDps.push({ date: formattedDate, holiday: "Arafat Day" });
                 count += 1;
             }
             if (holidaysData.Islamic_New_Year === 1) {
-                muslimDps.push({ date: holidaysData.date, holiday: "Islamic New Year" });
+                muslimDps.push({ date: formattedDate, holiday: "Islamic New Year" });
                 count += 1;
             }
             if (holidaysData.Sacrifice_Feast === 1) {
-                muslimDps.push({ date: holidaysData.date, holiday: "Sacrifice Feast" });
+                muslimDps.push({ date: formattedDate, holiday: "Sacrifice Feast" });
                 count += 1;
             }
             if (holidaysData.Prophet_Muhammad_Birthday === 1) {
-                muslimDps.push({ date: holidaysData.date, holiday: "Prophet Muhammad Birthday" });
+                muslimDps.push({ date: formattedDate, holiday: "Prophet Muhammad Birthday" });
                 count += 1;
             }
             if (holidaysData.Ramadan === 1) {
-                muslimDps.push({ date: holidaysData.date, holiday: "Ramadan" });
+                muslimDps.push({ date: formattedDate, holiday: "Ramadan" });
                 count += 1;
             }
             // Jewish holidays
             if (holidaysData.Passover === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Passover" });
+                jewishDps.push({ date: formattedDate, holiday: "Passover" });
                 count += 1;
             }
             if (holidaysData.Memorial_Day === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Memorial Day" });
+                jewishDps.push({ date: formattedDate, holiday: "Memorial Day" });
                 count += 1;
             }
             if (holidaysData.Independence_Day === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Independence Day" });
+                jewishDps.push({ date: formattedDate, holiday: "Independence Day" });
                 count += 1;
             }
             if (holidaysData.Lag_BOmer === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Lag BOmer" });
+                jewishDps.push({ date: formattedDate, holiday: "Lag BOmer" });
                 count += 1;
             }
             if (holidaysData.Shavuot === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Shavuot" });
+                jewishDps.push({ date: formattedDate, holiday: "Shavuot" });
                 count += 1;
             }
             if (holidaysData.Rosh_Hashanah === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Rosh Hashanah" });
+                jewishDps.push({ date: formattedDate, holiday: "Rosh Hashanah" });
                 count += 1;
             }
             if (holidaysData.Yom_Kippur === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Yom Kippur" });
+                jewishDps.push({ date: formattedDate, holiday: "Yom Kippur" });
                 count += 1;
             }
             if (holidaysData.Sukkot === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Sukkot" });
+                jewishDps.push({ date: formattedDate, holiday: "Sukkot" });
                 count += 1;
             }
             if (holidaysData.Hanukkah === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Hanukkah" });
+                jewishDps.push({ date: formattedDate, holiday: "Hanukkah" });
                 count += 1;
             }
             if (holidaysData.Purim === 1) {
-                jewishDps.push({ date: holidaysData.date, holiday: "Purim" });
+                jewishDps.push({ date: formattedDate, holiday: "Purim" });
                 count += 1;
             }
             // Christian holidays
             if (holidaysData.New_Year_Day === 1) {
-                christianDps.push({ date: holidaysData.date, holiday: "New Year Day" });
+                christianDps.push({ date: formattedDate, holiday: "New Year Day" });
                 count += 1;
             }
             if (holidaysData.Christmas_Day === 1) {
-                christianDps.push({ date: holidaysData.date, holiday: "Christmas Day" });
+                christianDps.push({ date: formattedDate, holiday: "Christmas Day" });
                 count += 1;
             }
             if (holidaysData.Easter === 1) {
-                christianDps.push({ date: holidaysData.date, holiday: "Easter" });
+                christianDps.push({ date: formattedDate, holiday: "Easter" });
                 count += 1;
             }
 
             if (count > 0) {
-                dps.push({ date: holidaysData.date, count: count });
+                dps.push({ date: formattedDate, count: count });
             }
         })
 
@@ -131,70 +132,49 @@ class HolidaysHeatmap extends Component {
             christianHolidays : christianDps
         });
     }
-
-    
-    getHolidaysDataPoints() {
-        var startDate = this.props.startDate;
-		var endDate = this.props.endDate;
-        
-        var dps = [
-            { date: '2016/01/15', count: 1 },
-            { date: '2016/01/16', count: 1 },
-            { date: '2016/01/17', count: 2 },
-            { date: '2016/02/01', count: 4 },
-            { date: '2016/02/02', count: 3 },
-            { date: '2016/02/10', count: 1 },
-            ];
-        return dps;
-	}
-    
     
     render() {
         return (
             <div>
                 <HeatMap
-                    value = {this.getHolidaysDataPoints()}
-                    //value = {this.state.dataPoints}
+                    value = {this.state.dataPoints}
                     rectSize = {16}
                     width = {140}
-                    panelColors= {{
+                    panelColors = {{
                         0: '#f2dbdb',
                         1: '#ff7779'
-                      }}
+                    }}
                     legendCellSize = {0}
-                    startDate = {this.state.startDate}
-                    endDate = {this.state.endDate}
+                    startDate = {new Date(this.props.startDate)}
+                    endDate = {new Date(this.props.endDate)}
                     rectRender = {(props, data) => {
                         var muslimHolidaysStr = "", jewishHolidaysStr = "", christianHolidaysStr = "";
-                        var dateStr = new Date(data.date).toString();
-                        var first = true;
-                        for(var i=0; i < this.state.muslimHolidays.length; i++) {
-                            if (new Date(this.state.muslimHolidays[i].date).toString() === dateStr) {
+                        var first = true, i;
+                        // Change date format
+                        var date = new Date(data.date);
+                        date.setDate(date.getDate() + 1);
+                        date = date.toISOString().slice(0, 10);
+                        date = date.split('-').join('/');
+
+                        for(i=0; i < this.state.muslimHolidays.length; i++) {
+                            if (this.state.muslimHolidays[i].date === date) {
                                 if (!first) {
                                     muslimHolidaysStr += ", "
                                 }
-                                muslimHolidaysStr += this.state.muslimHolidays[i].holiday
+                                muslimHolidaysStr += this.state.muslimHolidays[i].holiday;
                                 first = false
                             }
                         }
-                        for(var i=0; i < this.state.jewishHolidays.length; i++) {
-                            if (new Date(this.state.jewishHolidays[i].date).toString === dateStr) {
-                                jewishHolidaysStr += this.state.jewishHolidays[i].holiday
+                        for(i=0; i < this.state.jewishHolidays.length; i++) {
+                            if (this.state.jewishHolidays[i].date === date) {
+                                jewishHolidaysStr += this.state.jewishHolidays[i].holiday;
                             }
                         }
-                        for(var i=0; i < this.state.christianHolidays.length; i++) {
-                            if (new Date(this.state.christianHolidays[i].date).toString === dateStr) {
-                                christianHolidaysStr += this.state.christianHolidays[i].holiday
+                        for(i=0; i < this.state.christianHolidays.length; i++) {
+                            if (this.state.christianHolidays[i].date === date) {
+                                christianHolidaysStr += this.state.christianHolidays[i].holiday;
                             }
                         }
-
-                        /*
-                        if (new Date("2016/01/12").toString() === new Date(data.date).toString()) {
-                            muslimHolidaysStr = "Eid al-Fitr, Ramadan"
-                            christianHolidaysStr = "Christmas Day"
-                        }
-                        */
-
 
                         return (                           
                             <Tooltip
