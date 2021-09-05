5dee3b0750dc12d84ea8983e649ae3b1e87ba9b9
alumadavidi <alumadavidi@gmail.com>
2021-09-05 16:52:49 +0300

    up

--- src/Components/PredictionData.jsx
+++ src/Components/PredictionData.jsx
@@ -3,58 +3,84 @@ import "../Pages.css";
 
 import NoIcon from "@material-ui/icons/Clear";
 import YesIcon from "@material-ui/icons/Check";
+import HelpIcon from "@material-ui/icons/HelpOutline";
 import green from "@material-ui/core/colors/green";
+import HyperParams from "./HyperParams";
 
 class PredictionData extends Component {
   state = {
-    date: this.props.date,
-    pred: this.props.pred,
-    modelAcc: this.props.modelAcc,
-    params: this.props.params,
-    icon: <NoIcon style={{ fontSize: 100 }} color="secondary" />,
+    iconNo: <NoIcon style={{ fontSize: 100 }} color="secondary" />,
+    iconYes: <YesIcon style={{ color: green[500], fontSize: 100 }} />,
+    iconHelp: <HelpIcon style={{ fontSize: 100 }} />,
   };
   render() {
-    if (this.state.classPred) {
-      this.setState({
-        icon: <YesIcon style={{ color: green[500], fontSize: 100 }} />,
-      });
-    }
     return (
       <div className="PredData">
-        <h1 className="predTitle"> Predict Terror wave by date </h1>
         <i className="fas fa-star-and-crescent"></i>
 
         <div className="PredLeft">
           <div className="ModelsDiv">
+            <h3 className="PredTitle"> Model Prediction</h3>
+
             <div className="ModelView">
-              <h3 className="PredTitle"> Model Prediction</h3>
-              <div id="icon">{this.state.icon}</div>
+              <div id="icon">{this.getIcon(this.props.getPred())}</div>
             </div>
           </div>
           <div className="AccDiv">
+            <h3 className="PredTitle">Model accuracy</h3>
+
             <div className="ModelView">
-              <h3 className="PredTitle">Model accuracy</h3>
-              <h1 id="Acc">{this.state.modelAcc}</h1>
+              <h1 id="Acc">{this.getAcc(this.props.getModelAcc())}</h1>
+            </div>
+          </div>
+          <div className="AccDiv">
+            <h3 className="PredTitle">Test accuracy</h3>
+
+            <div className="ModelView">
+              <h1 id="Acc">{this.getAcc(this.props.getTestAcc())}</h1>
             </div>
           </div>
         </div>
 
+        <div className="PredDataRigh">
+          <h3 className="PredTitle"> Hyper Parameters</h3>
+
+          <HyperParams
+            getData={() => {
+              return this.props.getHyperParams();
+            }}
+          ></HyperParams>
+        </div>
         <div className="PredDataRight">
-          <ul className="ParamList">
-            {this.state.params.map((val, key) => {
-              return (
-                <li className="row" key={key}>
-                  <div id="icon">{val.icon}</div>
-                  <div id="title">{val.title}</div>
-                  <div id="val">{val.val}</div>
-                </li>
-              );
-            })}
-          </ul>
+          <h3 className="PredTitle"> Features</h3>
+
+          <HyperParams
+            getData={() => {
+              return this.props.getFeatures();
+            }}
+          ></HyperParams>
         </div>
       </div>
     );
   }
+
+  getIcon = (pred) => {
+    console.log(pred);
+    if (pred == undefined) {
+      return "";
+    } else if (pred) {
+      return this.state.iconYes;
+    } else {
+      return this.state.iconNo;
+    }
+  };
+  getAcc = (acc) => {
+    if (acc == undefined) {
+      return "";
+    } else {
+      return String(acc) + "%";
+    }
+  };
 }
 
 export default PredictionData;
--- src/Pages/Prediction.jsx
+++ src/Pages/Prediction.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "../Pages.css";
 import DatePicker from "../Components/DatePicker";
+import ConfusionMatrix from "../Components/ConfusionMatrix";
+import HyperParams from "../Components/HyperParams";
+
 import PredictionData from "../Components/PredictionData";
 import WeatherIcon from "@material-ui/icons/WbSunny";
 import HolidayIcon from "@material-ui/icons/AccountBalance";
@@ -9,36 +12,31 @@ import axios from "axios";
 
 class Prediction extends Component {
   state = {
+    hyperParams: {},
+    confusionMatrix: {},
+    features: {},
     date: new Date(),
-    pred: false,
-    modelAcc: 80,
-    params: [
-      {
-        title: "Israel weather",
-        icon: <MDBIcon icon="mosque" />,
-        val: 90,
-      },
-      { title: "Jewish holiday", icon: <HolidayIcon />, val: "-" },
-      { title: "Muslim holiday", icon: <WeatherIcon />, val: "-" },
-      { title: "israel election day", icon: <WeatherIcon />, val: "-" },
-      { title: "google trends", icon: <WeatherIcon />, val: "-" },
-    ],
+    pred: undefined,
+    modelAcc: undefined,
+    testAcc: undefined,
+    params: [],
   };
 
-  updateDate = (dateValue) => {
-    this.setState({ date: dateValue });
-    console.log(this.state.date);
-
-    let obj = {
-      username: `${this.state.inputUser}`,
-      password: `${this.state.inputPsw}`,
-    };
-
+  updateConfusionMatrix = (fullYear) => {
     axios
-      .post("/login", obj)
+      .get("/ConfusionMatrix", {
+        params: {
+          year: fullYear,
+        },
+      })
       .then((res) => {
         console.log(res.status);
         if (res.status === 200) {
+          this.setState({
+            confusionMatrix: res.data[0],
+          });
+
+          console.log(this.state);
         }
       })
       .catch((error) => {
@@ -50,16 +48,150 @@ class Prediction extends Component {
       });
   };
 
+  updateHyperParams = (fullYear) => {
+    axios
+      .get("/Hyperparmeters", {
+        params: {
+          year: fullYear,
+        },
+      })
+      .then((res) => {
+        console.log(res.status);
+        if (res.status === 200) {
+          console.log(res.data);
+          this.setState({
+            hyperParams: res.data[0],
+          });
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          alert("Invalid username or password");
+        } else {
+          alert("Error! Something went wrong - server faild");
+        }
+      });
+  };
+
+  updateFeatures = (fullYear) => {
+    axios
+      .get("/Features", {
+        params: {
+          year: fullYear,
+        },
+      })
+      .then((res) => {
+        console.log(res.status);
+        if (res.status === 200) {
+          console.log(res.data);
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          alert("Invalid username or password");
+        } else {
+          alert("Error! Something went wrong - server faild");
+        }
+      });
+  };
+
+  updateAcc = (fullYear) => {
+    axios
+      .get("/", {
+        params: {
+          year: fullYear,
+        },
+      })
+      .then((res) => {
+        console.log(res.status);
+        if (res.status === 200) {
+          console.log(res.data);
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          alert("Invalid username or password");
+        } else {
+          alert("Error! Something went wrong - server faild");
+        }
+      });
+  };
+
+  update = (dateValue) => {
+    this.setState({ date: new Date(dateValue) });
+    let dateTime = new Date(dateValue);
+    let fullYear = dateTime.getFullYear();
+    dateTime.setDate(dateTime.getDate() + 1);
+    this.updateConfusionMatrix(fullYear);
+    this.updateHyperParams(fullYear);
+    this.updateFeatures(fullYear);
+    this.updatePred(dateTime);
+    this.updateAcc(fullYear);
+
+    this.forceUpdate();
+  };
+  getConfusionMatrix = () => {
+    return this.state.confusionMatrix;
+  };
+  getHyperParams = () => {
+    return this.state.hyperParams;
+  };
+  getFeatures = () => {
+    return this.state.features;
+  };
+
+  getPred = () => {
+    return this.state.pred;
+  };
+
+  getModelAcc = () => {
+    return this.state.modelAcc;
+  };
+
+  getTestAcc = () => {
+    return this.state.testAcc;
+  };
+
+  getParams = () => {
+    return this.state.params;
+  };
+
+  getDate = () => {
+    return this.state.date;
+  };
+
   render() {
     return (
       <div>
-        <DatePicker updateDate={this.updateDate} />
+        <DatePicker updateDate={this.update} />
+        <ConfusionMatrix
+          getConfusionMatrix={() => {
+            return this.getConfusionMatrix();
+          }}
+        ></ConfusionMatrix>
 
         <PredictionData
-          date={this.state.date}
-          pred={this.state.classPred}
-          modelAcc={this.state.classModelAcc}
-          params={this.state.params}
+          getFeatures={() => {
+            return this.getFeatures();
+          }}
+          getHyperParams={() => {
+            return this.getHyperParams();
+          }}
+          getDate={() => {
+            return this.getDate();
+          }}
+          getPred={() => {
+            return this.getPred();
+          }}
+          getModelAcc={() => {
+            return this.getModelAcc();
+          }}
+          getTestAcc={() => {
+            return this.getTestAcc();
+          }}
+          getParams={() => {
+            return this.getParams();
+          }}
         />
       </div>
     );
