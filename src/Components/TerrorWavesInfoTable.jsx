d66b1fe5aa44053b75922d72e5decad5a3d644ff
shaked arbili <shaked.arbili@live.biu.ac.il>
2021-09-26 00:03:40 +0300

    terror waves page and images directory were added

---
+++ src/Components/TerrorWavesInfoTable.jsx
@@ -0,0 +1,71 @@
+import "../Image.css";
+import "../App.css";
+
+import React, { Component } from "react";
+import axios from "axios";
+
+class TerrorTableTest extends Component {
+  constructor() {
+    super();
+    this.state = {
+      info: [],
+    };
+  }
+
+  async getDataPoints() {
+    let attacksResponse = await axios.get("/TerrorWavesInfo");
+    var attacksInfo = [];
+    attacksResponse.data.map((attacksData) => {
+      attacksInfo.push({
+        start_date: attacksData.start_wave_date,
+        end_date: attacksData.end_wave_date,
+        img: attacksData.img_name,
+        desc: attacksData.description,
+      });
+    });
+
+    this.setState({
+      info: attacksInfo,
+    });
+  }
+
+  componentDidMount() {
+    this.getDataPoints();
+  }
+
+  render() {
+    // Arrange the attacks information in a table
+    const attacksImagesContainer = [];
+    //'<div class="imagesContainer">';
+    const attackImages = [];
+    for (var i = 0; i < this.state.info.length; i++) {
+      attackImages.push(
+        <div class="container">
+          <div class="overlay">
+            <div class="image_title">
+              {this.state.info[i].start_date} - {this.state.info[i].end_date}
+              <p class="image_description">{this.state.info[i].desc}</p>
+            </div>
+          </div>
+          <img
+            src={require("../images/" + this.state.info[i].img).default}
+            alt=""
+            class="image"
+          />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <div className="Page">
+          <h1>dskmk</h1>
+        </div>
+        <h1>Terror Waves Information</h1>
+        <div class="imagesContainer">{attackImages}</div>
+      </div>
+    );
+  }
+}
+
+export default TerrorTableTest;
--- src/Pages/TerrorWavesInformation.jsx
+++ src/Pages/TerrorWavesInformation.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-
+import TerrorWavesInfoTable from "../Components/TerrorWavesInfoTable";
+import "../App.css";
 
 class TerrorWavesInformation extends Component {
-    state = {};
-    render() {
-        return (
-            <div>
-                Terror Waves Information
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <TerrorWavesInfoTable />
+      </div>
+    );
+  }
 }
 
 export default TerrorWavesInformation;
